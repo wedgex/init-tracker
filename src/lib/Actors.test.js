be75dd07f1a3754f9baf38b3c586e3e2b1bc2b46
Hunter Haydel <haydh530@gmail.com>
2017-04-04 20:58:07 -0400

    Test create Actor

--- src/lib/Actors.test.js
+++ src/lib/Actors.test.js
@@ -1,4 +1,4 @@
-import { sort } from './Actors'
+import { create, sort } from './Actors'
 
 describe('sort', () => {
   it('sorts actors by initiative', () => {
@@ -13,5 +13,27 @@ describe('sort', () => {
       { init: 10, name: 'Snugglesworth' },
       { init: 1, name: 'Thorgrim' },
     ])
+  })
+})
+
+describe('create', () => {
+  it('generates a uuid', () => {
+    const actor = create({ name: 'K\'thriss' })
+    expect(actor.id).toBeTruthy()
+  })
+
+  it('has name', () => {
+    const actor = create({ name: 'K\'thriss' })
+    expect(actor.name).toEqual('K\'thriss')
+  })
+
+  it('parses init', () => {
+    const actor = create({ name: 'K\'thriss', init: '10' })
+    expect(actor.init).toEqual(10)
+  })
+
+  it('defaults init to 0', () => {
+    const actor = create({})
+    expect(actor.init).toEqual(0)
   })
 })
