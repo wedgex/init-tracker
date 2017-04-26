11684b4a307aa6e560020b993646554a874e86f6
Hunter Haydel <haydh530@gmail.com>
2017-04-25 22:53:46 -0400

    Add Redux Router

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React from 'react'
-import './App.css'
-import NewActorContainer from './NewActorContainer'
-import Actor from './Actor'
 import {
-  sort as sortActors,
-} from './lib/Actors'
-import { connect } from 'react-redux'
-import {
-  add as addActor,
-  remove as removeActor,
-  expand as expandActor,
-  selectActors,
-  selectExpanded,
-} from './actors'
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+import Root from './routes/Root'
 
-const App = ({ actors, addActor, expandActor, removeActor, expandedId }) => (
-  <div>
-    <NewActorContainer onSubmit={addActor} />
-    {
-      sortActors(actors).map((actor, i) => (
-        <Actor
-          key={i}
-          {...actor}
-          isSelected={ actor.id === expandedId}
-          onRemove={ () => removeActor(actor) }
-          onClick={ () => expandActor(actor) }
-        />
-      ))
-    }
-  </div>
+export default () => (
+  <Router>
+    <Route exact path="/" component={Root} />
+  </Router>
 )
-
-const mapStateToProps = state => ({
-  actors: selectActors(state),
-  expandedId: selectExpanded(state),
-})
-
-const mapDispatchToProps = {
-  addActor,
-  removeActor,
-  expandActor,
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ src/routes/Root.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import NewActorContainer from '../NewActorContainer'
+import Actor from '../Actor'
+import {
+  sort as sortActors,
+} from '../lib/Actors'
+import { connect } from 'react-redux'
+import {
+  add as addActor,
+  remove as removeActor,
+  expand as expandActor,
+  selectActors,
+  selectExpanded,
+} from '../actors'
+
+const Root = ({ actors, addActor, expandActor, removeActor, expandedId }) => (
+  <div>
+    <NewActorContainer onSubmit={addActor} />
+    {
+      sortActors(actors).map((actor, i) => (
+        <Actor
+          key={i}
+          {...actor}
+          isSelected={ actor.id === expandedId}
+          onRemove={ () => removeActor(actor) }
+          onClick={ () => expandActor(actor) }
+        />
+      ))
+    }
+  </div>
+)
+
+const mapStateToProps = state => ({
+  actors: selectActors(state),
+  expandedId: selectExpanded(state),
+})
+
+const mapDispatchToProps = {
+  addActor,
+  removeActor,
+  expandActor,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Root)
