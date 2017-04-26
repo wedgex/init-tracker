df6d565dc9ed7c28389e8e36591a60cf5d70d195
Hunter Haydel <haydh530@gmail.com>
2017-04-25 21:10:40 -0400

    Install Redux
    Currently has invalid reducer, but just setting up to migrate the
    functionality in the App component to redux.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducer'
 import './index.css';
 import 'bulma/css/bulma.css'
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <Root />,
+  <Provider store={store}>
+    <Root />
+  </Provider>,
   document.getElementById('root'),
 );
