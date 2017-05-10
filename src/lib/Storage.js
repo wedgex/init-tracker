7c8a3a46872e65022b4d26d091aa0b0daed9709d
Hunter Haydel <haydh530@gmail.com>
2017-05-10 19:29:55 -0400

    Remove lib/Storage

--- src/lib/Storage.js
+++
@@ -1,9 +0,0 @@
-export const ACTORS_STORE_KEY = 'actors'
-
-export const load = () => (
-  JSON.parse(window.localStorage.getItem(ACTORS_STORE_KEY)) || []
-)
-
-export const save = actors => {
-  window.localStorage.setItem(ACTORS_STORE_KEY, JSON.stringify(actors))
-}
